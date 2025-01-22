4ee4afd821205f393736f2af9fb76befd182294d
Nicolas JACOB-PERES <nicolasjp05@gmail.com>
2025-01-22 18:44:28 +0100

    ecriture data into file

--- Data/first_request.py
+++ Data/first_request.py
@@ -1,6 +1,7 @@
 from amadeus import Client, ResponseError
 from dotenv import load_dotenv
 import os
+import json
 
 # Charger les variables d'environnement depuis le fichier .env
 load_dotenv()
@@ -22,11 +23,21 @@ def main():
             destinationLocationCode='ATH',
             departureDate='2025-02-01',
             adults=1)
+    
         # print(response.body) #=> The raw response, as a string
-        # print(response.result) #=> The body parsed as JSON, if the result was parsable
-        print(response.data) #=> The list of locations, extracted from the JSON
+        # print(response.result) #=> The body parsed as JSON, if the result was parsable (on utilise celui là)
+            # Pour écrire dans un fichier, il est souvent préférable d'utiliser response.result car c'est plus structuré et manipulable.
+        # print(response.data) #=> The list of locations, extracted from the JSON 
+
+        data = response.result
+
+        with open('flight_offers_search.json', 'w', encoding="utf-8") as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+
+        print("Données sauvegardées dans 'flight_offers_search.json'.")
+
     except ResponseError as error:
-        print(error)
+        print(f"Erreur : {error}")
 
 if __name__ == '__main__':
     main()
